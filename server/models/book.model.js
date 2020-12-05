95109087bdc6865b6321ff5d3f2b2e263d3c7e30
Javi Fernandez <javierfernandezriv@gmail.com>
2020-12-05 11:06:54 +0100

    Model Book

---
+++ server/models/book.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    genre: {
+        type: String,
+        enum: ["Ciencia Ficción", "Novela negra", "Aventuras", "Misterio", "Infantiles", "Románticas", "Terror"],
+        required: true,
+    },
+
+    resume: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    chapters: {
+        type: [],
+    },
+
+    image: {
+        type: String,
+        default: "https://www.gruposertec.com/wp-content/uploads/2015/03/no_image_available.jpeg",
+
+    },
+
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+}, {
+    timestamps: true
+})
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
