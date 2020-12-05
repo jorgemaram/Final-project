f4feb5fd00e337c5eee9f231eb1aeaeedce2742d
Javi Fernandez <javierfernandezriv@gmail.com>
2020-12-05 18:19:02 +0100

    Sign Up

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,5 @@ module.exports = app => {
     // Base URLS
     app.use('/api/books', require('./book.routes.js'))
     app.use('/api/events', require('./events.routes.js'))
+    app.use('/api/auth', require('./auth.routes.js'))
 }
