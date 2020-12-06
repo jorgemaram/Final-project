9baec5c47fdd1241c749a72e00eb72aa77870a98
Javi Fernandez <javierfernandezriv@gmail.com>
2020-12-06 13:30:04 +0100

    Login

--- client/src/service/auth.service.js
+++ client/src/service/auth.service.js
@@ -5,11 +5,12 @@ export default class AuthService {
     constructor() {
         this.apiHandler = axios.create({
             baseURL: 'http://localhost:5000/api/auth',
+
         })
     }
 
     signup = credentials => this.apiHandler.post('/signup', credentials)
-    // login = credentials => this.apiHandler.post('/login', credentials)
-    // logout = () => this.apiHandler.post('/logout')
-    // isLoggedIn = () => this.apiHandler.get('/loggedin')
+    login = credentials => this.apiHandler.post('/login', credentials)
+    logout = () => this.apiHandler.post('/logout')
+    isLoggedIn = () => this.apiHandler.get('/loggedin')
 }
