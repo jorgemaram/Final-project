87a06aa185b2acd92e9d410b6c5ab477ea3dab10
Jorge Martin <jorgemaram@gmail.com>
2020-12-07 19:36:33 +0100

    delete book done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import AuthServices from './../service/auth.service'
 import BookList from './pages/Book-list/Books-list'
 import BookDetails from './pages/Book-details/Book-details'
 import BookForm from './pages/Book-form/Book-form'
+import BookEditForm from './pages/Book-edit-form/Book-edit-form'
 import Signup from './pages/Signup/Signup'
 import Login from './pages/Login/Login'
 import Profile from './pages/Profile/Profile'
@@ -60,9 +61,9 @@ class App extends Component {
             <Route path="/acceso-usuario" render={props => <Login storeUser={this.setTheUser} {...props} />} />
             <Route path="/perfil" render={() => this.state.loggedInUser ? <Profile loggedUser={this.state.loggedInUser} /> : <Redirect to="/acceso-usuario" />} />
             <Route path="/libros" exact render={() => <BookList/>} />
-            <Route path="/libros/:book_id" render={props => <BookDetails {...props} />} />
-            <Route path="/libros/editar/:book_id" render={props => <BookDetails {...props} />} />
-            <Route path="/crear" render={props => <BookForm {...props}/> } />
+            <Route path="/libros/editar/:book_id" render={props => this.state.loggedInUser ? <BookEditForm {...props} loggedUser={this.state.loggedInUser} /> : <Redirect to="/acceso-usuario" />}/>
+            <Route path="/libros/:book_id" exact render={props => <BookDetails {...props} />} />
+            <Route path="/crear" render={props => this.state.loggedInUser ? <BookForm {...props} loggedUser={this.state.loggedInUser} /> : <Redirect to="/acceso-usuario" />} />
           </Switch>
         </main>
       </>
--- client/src/components/pages/Book-details/Book-details.js
+++ client/src/components/pages/Book-details/Book-details.js
@@ -5,7 +5,7 @@ import './Book-details.css'
 
 //si ponemos loader, irá aquí 
 
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
 
@@ -18,36 +18,54 @@ class BookDetails extends Component {
         }
         this.bookService = new BooksService()
     }
-    
+
     componentDidMount = () => {
-        
-        
+
+
         const book_id = this.props.match.params.book_id
-        
+
         this.bookService
-        .getBook(book_id)
+            .getBook(book_id)
             .then(res => this.setState({ book: res.data }))
             .catch(err => console.log(err))
+    }
+
+    deleteThisBook = () => {
+        const book_id = this.props.match.params.book_id
+
+        console.log('hola boton de eliminar')
+        this.bookService
+            .then(res => {
+                this.props.history.push('/libros')
+            })
+            .catch(err => console.log(err))
+        
     }
 
     render() {
 
         return (
-            <Container className="book-details">
-                <h1>Detalles {this.state.book.title}</h1>
-                <Row>
-                    <Col md={{ span: 6, offset: 1 }} >
-                        <img src={this.state.book.image} alt={this.state.book.title} />
-                    </Col>
-                    <Col md={4}>
-                        <h3>Detalles</h3>
-                        <p>{this.state.book.resume}</p>
-                        <hr />
-                        <p>Género: {this.state.book.genre}</p>
-                        <Link to="/libros" className="btn btn-sm btn-dark">Volver</Link>
-                    </Col>
-                </Row>
-            </Container>
+            <>
+                <Container>
+                    <h1>{this.state.book.title}</h1>
+                </Container>
+                <Container className="book-details">
+                    <Row>
+                        <Col md={{ span: 6, offset: 1 }} >
+                            <img src={this.state.book.image} alt={this.state.book.title} />
+                            <h3>Detalles</h3>
+                            <p>{this.state.book.resume}</p>
+                            <hr />
+                            <p>Género: {this.state.book.genre}</p>
+                            <Link to="/libros" className="btn btn-sm btn-dark">Volver</Link>
+                            <Button onClick={() => this.deleteThisBook()} className="btn btn-sm btn-danger">Borrar</Button>
+                        </Col>
+                        <Col md={4}>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+
         )
     }
 }
